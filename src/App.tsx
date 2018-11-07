90dff57e7254c5f9e400c0ecf00181bc5c62d36d
angelsol299 <angelsol299@hotmail.com>
2018-11-07 21:09:31 +0100

    functional
    not yet finished

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import "./App.css";
 
 import logo from "./logo.svg";
 
+type ONGOING_GAME = -1;
+
+const ONGOING_GAME = -1;
+
 const enum Player {
   None = 0,
   One = 1,
@@ -12,6 +16,7 @@ const enum Player {
 interface IState {
   board: Player[];
   nextPlayerTurn: Player;
+  gameIsWon: Player | ONGOING_GAME;
 }
 
 class App extends React.Component<{}, IState> {
@@ -27,23 +32,103 @@ class App extends React.Component<{}, IState> {
       Player.None,
       Player.None
     ],
+    gameIsWon: ONGOING_GAME,
     nextPlayerTurn: Player.One
+  };
+
+  public checkIfGameIsOver = (board: Player[]) => {
+    if (
+      board[0] === board[1] &&
+      board[1] === board[2] &&
+      board[2] !== Player.None
+    ) {
+      return board[0];
+    } else if (
+      board[3] === board[4] &&
+      board[4] === board[5] &&
+      board[5] !== Player.None
+    ) {
+      return board[3];
+    } else if (
+      board[6] === board[7] &&
+      board[7] === board[8] &&
+      board[8] !== Player.None
+    ) {
+      return board[6];
+    } else if (
+      board[0] === board[3] &&
+      board[3] === board[6] &&
+      board[6] !== Player.None
+    ) {
+      return board[0];
+    } else if (
+      board[1] === board[4] &&
+      board[4] === board[7] &&
+      board[7] !== Player.None
+    ) {
+      return board[1];
+    } else if (
+      board[2] === board[5] &&
+      board[5] === board[8] &&
+      board[8] !== Player.None
+    ) {
+      return board[2];
+    } else if (
+      board[0] === board[4] &&
+      board[4] === board[8] &&
+      board[8] !== Player.None
+    ) {
+      return board[0];
+    } else if (
+      board[2] === board[4] &&
+      board[4] === board[6] &&
+      board[6] !== Player.None
+    ) {
+      return board[2];
+    }
+    return -1;
   };
 
   public createOnClickHandler = (index: number) => () => {
     const { board, nextPlayerTurn } = this.state;
+
+    if (board[index] !== Player.None) {
+      return;
+    }
     const newBoard = board.slice();
     newBoard[index] = nextPlayerTurn;
+    const gameIsWon = this.checkIfGameIsOver(newBoard);
 
     this.setState({
       board: newBoard,
+      gameIsWon,
       nextPlayerTurn: 3 - nextPlayerTurn
     });
   };
 
   public renderCell = (index: number) => {
+    const { board } = this.state;
     return (
-      <div className="cell" onClick={() => this.createOnClickHandler(index)} />
+      <div
+        className="cell"
+        onClick={this.createOnClickHandler(index)}
+        data-player={board[index]}
+      />
+    );
+  };
+
+  public renderStatus = () => {
+    const { gameIsWon } = this.state;
+    const winningText =
+      gameIsWon !== Player.None
+        ? `Player ${gameIsWon} won`
+        : "The game is a draw!";
+    return (
+      <div style={{ marginTop: "30px" }}>
+        {"Player 1 is green"} <br />
+        {"Player 2 is red"} <br />
+        {gameIsWon === ONGOING_GAME ? "game is ongoing" : winningText}
+      </div>
     );
   };
 
@@ -64,8 +149,8 @@ class App extends React.Component<{}, IState> {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Tic Tact Toe with React JS</h1>
         </header>
-
         {this.renderBoard()}
+        {this.renderStatus()}
       </div>
     );
   }
